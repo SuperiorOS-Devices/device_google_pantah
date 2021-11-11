c3c97045607fab73002efa3e7bc9ae23b4a31859
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-12 04:48:12 +0530

    device-calyx: Set TARGET_PREBUILT_KERNEL
    Change-Id: I156a4af77bd619f855f734983b24677531156c4d

--- device-calyx.mk
+++ device-calyx.mk
@@ -9,3 +9,5 @@ PRODUCT_COPY_FILES += \
 # For Google Camera
 PRODUCT_COPY_FILES += \
     device/google/pantah/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
+
+TARGET_PREBUILT_KERNEL := device/google/pantah-kernel/Image.lz4
