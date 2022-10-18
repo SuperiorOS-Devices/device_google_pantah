2f0b51b0082600b0d92340ed086e50d298658ecc
Michael Bestas <mkbestas@gmail.com>
2022-10-19 01:26:50 +0300

    Extract EUICC permissions from stock /system_ext
    Change-Id: Ibcc9db9f219b005c787cee68042ffc1ca51c02d8

--- device-superior.mk
+++ device-superior.mk
@@ -3,8 +3,7 @@ DEVICE_PACKAGE_OVERLAYS += device/google/pantah/overlay-superior
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
-    device/google/pantah/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml \
-    device/google/pantah/permissions/permissions_com.google.euiccpixel.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.google.euiccpixel.xml
+    device/google/pantah/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
 
 # For Google Camera
 PRODUCT_COPY_FILES += \
