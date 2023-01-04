f2fe2f1221a44682c9590bc3bdd15afe19898acb
Michael Bestas <mkbestas@gmail.com>
2023-01-04 03:59:03 +0200

    Update fingerprint/build description from TQ1A.230105.002
    Change-Id: Ib1e4645e7757f4c2053c34342dabeda9316ff677

--- superior_panther.mk
+++ superior_panther.mk
@@ -13,9 +13,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=panther \
-    PRIVATE_BUILD_DESC="panther-user 13 TQ1A.221205.011 9244662 release-keys"
+    PRIVATE_BUILD_DESC="panther-user 13 TQ1A.230105.001.A2 9325585 release-keys"
 
-BUILD_FINGERPRINT := google/panther/panther:13/TQ1A.221205.011/9244662:user/release-keys
+BUILD_FINGERPRINT := google/panther/panther:13/TQ1A.230105.001.A2/9325585:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
