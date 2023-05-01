458a8be0984c13c2ca31d380db428e35473eeafe
Chirayu Desai <chirayudesai1@gmail.com>
2023-05-02 01:57:28 +0530

    Update fingerprint/build description from TQ2A.230505.002
    Change-Id: Ibf3857a706dc813cf1a71e951bd01e4a1d134cfe

--- superior_cheetah.mk
+++ superior_cheetah.mk
@@ -13,9 +13,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=cheetah \
-    PRIVATE_BUILD_DESC="cheetah-user 13 TQ2A.230405.003.E1 9802792 release-keys"
+    PRIVATE_BUILD_DESC="cheetah-user 13 TQ2A.230505.002 9891397 release-keys"
 
-BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ2A.230405.003.E1/9802792:user/release-keys
+BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ2A.230505.002/9891397:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
