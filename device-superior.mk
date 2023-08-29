95f07d7eaebdf5146ff422498548a5572a6c2583
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2023-08-29 11:00:02 +0000

    Revert "pantah: Drop HbmSVManagerOverlay and related files"
    This reverts commit 5a428e705759d29b7164f6e5d61ff0de181ca99e.

--- device-superior.mk
+++ device-superior.mk
@@ -12,6 +12,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/google/pantah/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
 
+# HBM
+PRODUCT_COPY_FILES += \
+    device/google/pantah/permissions/permissions_com.android.hbmsvmanager.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.android.hbmsvmanager.xml
+
 TARGET_PREBUILT_KERNEL := device/google/pantah-kernel/Image.lz4
 
 # Build necessary packages for vendor
