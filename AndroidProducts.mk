33b9387d695400cd29834a33c34e120e2f28d49d
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2023-12-06 15:34:26 +0530

    pantah: Adapt to SuperiorOS
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -24,7 +24,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_panther.mk \
     $(LOCAL_DIR)/factory_panther.mk \
     $(LOCAL_DIR)/lineage_cheetah.mk \
-    $(LOCAL_DIR)/lineage_panther.mk
+    $(LOCAL_DIR)/superior_panther.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_cloudripper-userdebug \
--- superior_panther.mk
+++ superior_panther.mk
@@ -6,7 +6,7 @@
 
 # Inherit some common Lineage stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_panther.mk)
@@ -17,7 +17,7 @@ include device/google/pantah/panther/device-lineage.mk
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 7
-PRODUCT_NAME := lineage_panther
+PRODUCT_NAME := superior_panther
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
